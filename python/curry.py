d1b82c9b5ce3b184ff0a8117f07efa435c82d741
BigZeeke <50774520+BigZeeke@users.noreply.github.com>
2026-06-15 19:37:54 -0700

    Homemade Python challenge to try and get currying locked in

--- python/curry.py
+++ python/curry.py
@@ -1,3 +1,9 @@
-def curry(fn):
-    # Write code here
-    pass
+def batting_average(player):
+    def get_games(games):
+        def get_hits(hits):
+            def get_at_bats(at_bats):
+                avg = hits / at_bats
+                return f"{player} had {at_bats} at bats in {games} games, with {hits} hits and a{avg: .3f} batting average"
+            return get_at_bats
+        return get_hits
+    return get_games
--- python/curry_tests.py
+++ python/curry_tests.py
@@ -1,26 +1,37 @@
-from curry import curry
+from curry import batting_average
 
 
-def add3(a, b, c):
-    return a + b + c
+print(batting_average("Ohtani")(8)(7)(20))
+print(batting_average("Trout")(10)(9)(30))
+print(batting_average("Betts")(15)(12)(45))
+
+lopez = batting_average("Lopez")(162)
+print(lopez(45)(150))
+print(lopez(38)(120))
+print(lopez(60)(200))
+
+# Didn't get to the examples below, I was just trying to get it to click
+
+# def add3(a, b, c):
+#     return a + b + c
 
 
-def mul2(a, b):
-    return a * b
+# def mul2(a, b):
+#     return a * b
 
 
-def add4(a, b, c, d):
-    return a + b + c + d
+# def add4(a, b, c, d):
+#     return a + b + c + d
 
 
-curried_add = curry(add3)
-print(curried_add(1)(2)(3) == 6)
-print(curried_add(10)(20)(30) == 60)
-print(curried_add(-5)(5)(0) == 0)
+# curried_add = curry(add3)
+# print(curried_add(1)(2)(3) == 6)
+# print(curried_add(10)(20)(30) == 60)
+# print(curried_add(-5)(5)(0) == 0)
 
-curried_mul = curry(mul2)
-print(curried_mul(4)(5) == 20)
-print(curried_mul(0)(100) == 0)
+# curried_mul = curry(mul2)
+# print(curried_mul(4)(5) == 20)
+# print(curried_mul(0)(100) == 0)
 
-curried_add4 = curry(add4)
-print(curried_add4(1)(2)(3)(4) == 10)
+# curried_add4 = curry(add4)
+# print(curried_add4(1)(2)(3)(4) == 10)
